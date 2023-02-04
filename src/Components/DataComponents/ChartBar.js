d443104fce0561392a26ca8941d8f5ab53c2ecb6
Amin Ansari <devansari@yahoo.com>
2023-02-04 18:12:12 +0330

    Height of chart bars are set based on percend now

--- src/Components/DataComponents/ChartBar.js
+++ src/Components/DataComponents/ChartBar.js
@@ -3,7 +3,7 @@ import "./ChartBar.css";
 export default function ChartBar(props) {
   let barHeight = "0%";
   if (props.value > 0) {
-    barHeight = Math.floor((props.value / props.maxVal) * 100);
+    barHeight = `${Math.floor((props.value / props.maxVal) * 100)}%`;
   }
   return (
     <div className="bar-container">
